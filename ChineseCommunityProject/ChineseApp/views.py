62745774a4afd0bfb27538c19afdf6249396fde6
Yejin_Cho <yy991125@naver.com>
2019-07-22 18:51:41 +0900

    Django Project for mac  initialize
    장고 버전 2.1.3
    파이썬 3.6.5
    프로젝트명 ChineseCommunityProject
    앱 명 ChineseApp
    템플릿 폴더 명 templates
    메인 html: main.html

---
+++ ChineseCommunityProject/ChineseApp/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+def func(request):
+    return render(request, 'main.html')
+# Create your views here.
---
+++ myVenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/yejincho/Desktop/LikeLion_ChineseCommunity/myVenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

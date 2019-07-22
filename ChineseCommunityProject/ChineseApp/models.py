be1428b37d487342bf15a72d25480049f7c47501
Yejin_Cho <yy991125@naver.com>
2019-07-22 19:11:54 +0900

    made models.py & migrate & installed Pillow

--- ChineseCommunityProject/ChineseApp/models.py
+++ ChineseCommunityProject/ChineseApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Posting(models.Model):
+    title = models.CharField(max_length = 200)
+    pubDate = models.DateTimeField('date published')
+    body = models.TextField()
+    nickName = models.CharField(max_length = 20)
+    isNotice = models.BooleanField()
+    image = models.ImageField()
+    #uuid = models.UUIDField()
+
+    
